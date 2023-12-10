ce45e1ee2eda45a2a848157030ba80310381d379
biskall <itaibiskall@gmail.com>
2023-12-10 23:52:20 +0200

    api key

--- src/store/Api/AllApi.tsx
+++ src/store/Api/AllApi.tsx
@@ -1,4 +1,4 @@
-const API_KEY = 'QS5Wvqy7ysiHVk86Z23d9adpwdzmh9Oj'
+const API_KEY = '	aZaYaTNyMK35eSVfR68kWlwTHTywGMQM'
 const BASE_URL = `https://dataservice.accuweather.com`
 
 
@@ -6,10 +6,10 @@ export const weatherByKey = (key: string) => {
     return `${BASE_URL}/currentconditions/v1/${key}?apikey=${API_KEY}`
 }
 
-export const autocomplete = (wordKey: string) => {
+export const autocomplete =(wordKey:string) => {
     return `${BASE_URL}/locations/v1/cities/autocomplete?apikey=${API_KEY}&q=${wordKey}`
 }
 
-export const forecastsByCityKey = (cityKey: string, isCelsius = true) => {
+export const forecastsByCityKey =(cityKey:string, isCelsius = true) => {
     return `${BASE_URL}/forecasts/v1/daily/5day/${cityKey}?apikey=${API_KEY}&metric=${isCelsius}`
 }
